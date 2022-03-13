8d40fcba18d09b55194a49a8e2dfaa936d4837d8
Marty Lyon <martylyon@martys-mbp.home>
2022-03-13 15:58:23 +0000

    Updated index, style and script files

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value
+    y = document.getElementById("years").value
+
+    let  currentyear =  new Date().getFullYear() + parseInt(y);
+
+    document.getElementById("result").innerHTML = "If  you deposit <mark>" + p + "</mark>,<br /> at an interest rate of <mark>" + r + "%</mark>. <br /> You will receive an amount of <mark>" + (p * (r/100))* y + "</mark>, <br />in the year <mark>" + currentyear + "</mark>.";
     
 }
-        
+
+
+    
+function updateTextInput(val) {
+          document.getElementById('textInput').value=parseFloat(val).toFixed(2);
+
+        }
